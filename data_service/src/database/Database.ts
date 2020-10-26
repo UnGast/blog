ebc3489f874b401ea932a2c153741dc6d0b83d22
UnGast <zadrian@web.de>
2020-10-26 13:02:11 +0100

    in dev mode reread content on every request

--- data_service/src/database/Database.ts
+++ data_service/src/database/Database.ts
@@ -3,26 +3,23 @@ import FSProjectManager from '@/models/projects/FSProjectManager'
 import Project from '@/models/projects/Project'
 
 class Database {
-
   projectManager = new FSProjectManager()
-
   initPromise: Promise<any>
-
   projects: Project[]
 
   constructor() {
-
     this.initPromise = (async () => {
-
       this.projects = await this.projectManager.readAllProjects(path.resolve(process.env.CONTENT_DIR_PATH, 'projects'))
     })()
   }
 
   async getProjects() {
-
-    await this.initPromise
-
-    return this.projects
+    if (process.env.NODE_ENV === 'development') {
+      return await this.projectManager.readAllProjects(path.resolve(process.env.CONTENT_DIR_PATH, 'projects'))
+    } else {
+      await this.initPromise
+      return this.projects
+    }
   }
 }
 
